fcb0a9d875ba9c69617b191ee56a274979d81be7
eltoncelestino <eltongf2014@gmail.com>
2017-12-07 19:27:27 -0300

    Update user_tela.sh

--- user_tela.sh
+++ user_tela.sh
@@ -2,13 +2,11 @@
 rm -rf conversas.txt chatzao.txt primeirao.txt
 u=$1
 ip=$2
-echo $u > usr
 clear
 hose $ip 8801 --netslave > primeirao.txt 2> /dev/null
 while true
 do
         hose $ip 8801 --netslave > chatzao.txt 2> /dev/null
-#       tput cup 30 0
         diff -i chatzao.txt primeirao.txt | grep \< | cut -d'<' -f 2 | sed "s/^ $u/Você/" >> conversas.txt
         var=$(diff -i chatzao.txt primeirao.txt | grep \< | cut -d'<' -f 2 |head -n1)
         if [ ! -z "$var" ]
